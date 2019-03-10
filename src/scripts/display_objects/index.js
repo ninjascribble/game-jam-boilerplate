ad1b066d8d7958aa573f01a172ff09886f9e1eb7
Scott Grogan <scott@ninjascript.com>
2019-03-10 14:02:50 -0700

    Fix text alignment

--- src/scripts/display_objects/index.js
+++ src/scripts/display_objects/index.js
@@ -1,4 +1,4 @@
-import BitmapFont from './BitmapFont';
+import { GameObjects } from 'Phaser';
 
 const DISPLAY_FONT = 'Blocktopia_32pt';
 const BODY_FONT = 'Blocktopia_12pt';
@@ -9,11 +9,35 @@ module.exports = {
     loader.bitmapFont(BODY_FONT, 'Blocktopia_12pt.png', 'Blocktopia_12pt.fnt');
   },
 
-  displayFont: function displayFont (scene, text = '', x = 0, y = 0, align = 'left') {
-    return new BitmapFont(scene, x, y, DISPLAY_FONT, text, 30, align);
+  displayFont: function displayFont (scene, text = '', x = 0, y = 0, align = 0) {
+    const font = new GameObjects.BitmapText(scene, x, y, DISPLAY_FONT, text, 30, align);
+    setBitmapTextOrigin(font, align);
+    return font;
   },
 
-  bodyFont: function displayFont (scene, text = '', x = 0, y = 0, align = 'left') {
-    return new BitmapFont(scene, x, y, BODY_FONT, text, 12, align);
+  bodyFont: function displayFont (scene, text = '', x = 0, y = 0, align = 0) {
+    const font = new GameObjects.BitmapText(scene, x, y, BODY_FONT, text, 12, align);
+    setBitmapTextOrigin(font, align);
+    return font;
   }
 };
+
+
+
+/**
+ * @override Phaser.BitmapText._align
+ */
+function setBitmapTextOrigin(bitmapText, align) {
+  switch (align) {
+  case GameObjects.BitmapText.ALIGN_CENTER:
+    bitmapText.setOrigin(0.5, 0.5);
+    break;
+  case GameObjects.BitmapText.ALIGN_RIGHT:
+    bitmapText.setOrigin(1, 0.5);
+    break;
+  case GameObjects.BitmapText.ALIGN_LEFT:
+  default:
+    bitmapText.setOrigin(0, 0.5);
+    break;
+  }
+}
--- src/scripts/scenes/Menu.js
+++ src/scripts/scenes/Menu.js
@@ -1,5 +1,5 @@
 import DisplayObjects from '../display_objects';
-import { Input, Scene } from 'Phaser';
+import { GameObjects, Input, Scene } from 'Phaser';
 
 export default class Menu extends Scene {
   constructor () {
@@ -14,20 +14,20 @@ export default class Menu extends Scene {
   }
 
   titleText () {
-    return DisplayObjects.displayFont(this, 'THIS IS THE MENU', this.cameras.main.centerX, 100, 'center');
+    return DisplayObjects.displayFont(this, 'THIS IS THE MENU', this.cameras.main.centerX, 100, GameObjects.BitmapText.ALIGN_CENTER);
   }
 
   alphabetText () {
     var text = DisplayObjects.bodyFont(this, `
 AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz
 1,234,567,890 Ti Tj To 77 71 73 91910 .:;,
-!№;%:?*()_+-=.,/|"'@#$^&{}[]`, this.cameras.main.centerX, 145, 'center');
+!№;%:?*()_+-=.,/|"'@#$^&{}[]`, this.cameras.main.centerX, 145, GameObjects.BitmapText.ALIGN_CENTER);
     text.maxWidth = 300;
     return text;
   }
 
   actionText () {
-    var text = DisplayObjects.bodyFont(this, 'Press Spacebar to Play!', this.cameras.main.centerX, 190, 'center');
+    var text = DisplayObjects.bodyFont(this, 'Press Spacebar to Play!', this.cameras.main.centerX, 190, GameObjects.BitmapText.ALIGN_CENTER);
     this.time.addEvent({
       loop: true,
       delay: 400,
